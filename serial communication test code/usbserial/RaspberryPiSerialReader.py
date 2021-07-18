70e646059d21e195914de5f178e3fa9a7fab06ac
Raghav <62105787+MythicalCow@users.noreply.github.com>
2021-07-18 16:07:55 -0400

    Delete serial communication test code/usbserial directory

--- serial communication test code/usbserial/RaspberryPiSerialReader.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import serial
-if __name__ == '__main__':
-    ser = serial.Serial('/dev/tty.usbserial-01BE27D0', 115200, timeout=1) #Declaring Serial Object. First parameter is the usb port. Second is Baud Rate.
-    ser.flush()
-    while True:
-        if ser.in_waiting > 0:
-            line = ser.readline().decode('utf-8').rstrip() #Stores serial data in utf 8 format.
-            print(line) 
