f93a2fff465c1231f1a43c6182b8a865107b7461
Vrishak Vemuri <53585843+FluffyCube9343@users.noreply.github.com>
2023-01-14 15:03:44 -0500

    Merge Pi<->Arduino and Gui<->Pi

--- RaspPi-flask-recieve/server.py
+++ RaspPi-flask-recieve/server.py
@@ -5,7 +5,7 @@ import serial
 import time
 
 ser = serial.Serial('/dev/ttyACM0', 9600, timeout=2)
-
+ser.flush()
 
 host = '0.0.0.0'
 port = 2345
@@ -19,17 +19,14 @@ while True:
 
     print("received data:", data)
     # Get rid of garbage/incomplete data
-    ser.flush()
+    
 
     # Infinite loop
-    i = 0
-    while (1):
-        i += 1
-        send_string = ("Data\n")
-        ser.write(send_string.encode('utf-8'))
-        time.sleep(0.01)
-        receive_string = ser.readline().decode('utf-8').rstrip()
-        print(receive_string)
 
-    conn.send(data)
+    ser.write(data)
+    time.sleep(0.01)
+    receive_string = ser.readline().decode('utf-8').rstrip()
+    print(receive_string)
+
+    conn.send(receive_string.encode())
     conn.close()
